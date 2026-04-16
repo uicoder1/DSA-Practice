9af374c4e6c7fed7a84722fc5bbfe594a7323be0
uicoder1 <179695918+uicoder1@users.noreply.github.com>
2026-04-16 22:32:11 +0530

    pascal traingle

---
+++ JV/pascaltriangle.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class pascaltriangle {
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+            }
+
+            result.add(row);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int numRows = 5;
+        System.out.println(generate(numRows));
+    }
+}
