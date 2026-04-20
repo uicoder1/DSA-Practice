61f4fed98fb9ec29f6603e559a53aff246e59988
uicoder1 <179695918+uicoder1@users.noreply.github.com>
2026-04-21 04:02:58 +0530

    jump game

---
+++ JV/jumpgame.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class jumpgame {
+
+    public static boolean canJump(int[] nums) {
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        System.out.println(canJump(nums)); // true
+    }
+}
