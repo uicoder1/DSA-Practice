8ccb12f95db8c15a6edc229d8ae9dcd2d5d525e1
uicoder1 <179695918+uicoder1@users.noreply.github.com>
2026-05-12 23:22:54 +0530

    sudoku

---
+++ JV/sukodu.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class sukodu {
+
+    public static boolean isValidSudoku(char[][] board) {
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                char num = board[i][j];
+
+                if (num == '.') continue;
+
+                String row = num + " in row " + i;
+                String col = num + " in col " + j;
+                String box = num + " in box " + (i / 3) + "-" + (j / 3);
+
+                if (set.contains(row) ||
+                    set.contains(col) ||
+                    set.contains(box)) {
+                    return false;
+                }
+
+                set.add(row);
+                set.add(col);
+                set.add(box);
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        System.out.println(isValidSudoku(board)); // true
+    }
+}
