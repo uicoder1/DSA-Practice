d7c0d94e1a38c866fb8696b2cb147ba4319b6c5c
uicoder1 <179695918+uicoder1@users.noreply.github.com>
2026-05-13 22:11:08 +0530

    insert interval

---
+++ JV/insert_interval.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class insert_interval {
+
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+
+        List<int[]> result = new ArrayList<>();
+
+        int i = 0;
+        int n = intervals.length;
+
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        result.add(newInterval);
+
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public static void main(String[] args) {
+
+        int[][] intervals = {{1,3},{6,9}};
+        int[] newInterval = {2,5};
+
+        int[][] result = insert(intervals, newInterval);
+
+        for (int[] interval : result) {
+            System.out.println(Arrays.toString(interval));
+        }
+    }
+}
